803c0ea75b15d6e9ed0a82521218563dfd338b19
Michal Kozubik <michal.kozubik@sap.com>
2021-08-31 10:42:10 +0200

    Allow different ports from the same hostname

--- dist/security/sameOrigin.js
+++ dist/security/sameOrigin.js
@@ -1,15 +1,20 @@
 "use strict";
 exports.__esModule = true;
+var withoutPort = function (url) {
+    return url.replace(/:\d+$/, "");
+};
 function default_1() {
     return function (request, response, next) {
-        var allowedHosts = new Set([request.headers.host]);
-        var referer = request.headers.host;
+        var allowedHosts = new Set([
+            withoutPort(request.headers.host)
+        ]);
+        var referer = withoutPort(request.headers.host);
         var origin = null;
         if (request.headers.referer) {
-            referer = new URL(request.headers.referer).host;
+            referer = withoutPort(new URL(request.headers.referer).host);
         }
         if (request.headers.origin) {
-            origin = new URL(request.headers.origin).host;
+            origin = withoutPort(new URL(request.headers.origin).host);
         }
         if (!allowedHosts.has((origin || referer))) {
             return next(new Error('Unallowed origin'));
--- src/security/sameOrigin.ts
+++ src/security/sameOrigin.ts
@@ -1,14 +1,20 @@
+const withoutPort = function (url) {
+	return url.replace(/:\d+$/, "");
+}
+
 export default function () {
 	return (request, response, next) => {
-		const allowedHosts = new Set([request.headers.host]);
-		let referer = request.headers.host;
+		const allowedHosts = new Set([
+			withoutPort(request.headers.host)
+		]);
+		let referer = withoutPort(request.headers.host);
 		let origin = null;
 
 		if (request.headers.referer) {
-			referer = new URL(request.headers.referer).host;
+			referer = withoutPort(new URL(request.headers.referer).host);
 		}
 		if (request.headers.origin) {
-			origin = new URL(request.headers.origin).host;
+			origin = withoutPort(new URL(request.headers.origin).host);
 		}
 
 		if (!allowedHosts.has((origin || referer))) {
